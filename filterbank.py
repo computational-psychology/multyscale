ff78fa7b5aae4b93a25b2841cdfb8df938e31219
Joris Vincent <joris.vincent@gmail.com>
2020-11-11 11:02:40 +0100

    ODOG filter bank function

---
+++ filterbank.py
@@ -0,0 +1,23 @@
+import numpy as np
+import filters
+
+
+def odog_bank(orientations,
+              sigmas,
+              x, y):
+    # TODO: docstring
+    # TODO: typehints
+    # TODO: refactor as class
+    # TODO: don't store filtes as ND-array
+    bank = np.empty(len(orientations),
+                    len(sigmas),
+                    x.shape[0], x.shape[1])
+
+    for i, angle in enumerate(orientations):
+        for j, sigma in enumerate(sigmas):
+            odog = filters.odog(x, y,
+                                sigma,
+                                (angle, angle))
+            bank[i, j, :, :] = odog
+
+    return bank
