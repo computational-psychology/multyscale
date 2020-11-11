13db981bc4bebebe968865e17c2702d9e3c4dc19
Joris Vincent <joris.vincent@gmail.com>
2020-11-11 16:11:06 +0100

    Model method for summing over spatial scales

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import numpy as np
 import filterbank
 
 # TODO: refactor filter-output datastructures
@@ -12,3 +13,15 @@ class ODOG_BM1999:
         self.visextent = visextent
 
         self.bank = filterbank.BM1999(shape, visextent)
+
+        center_sigmas = [sigma[0][0] for sigma in self.bank.sigmas]
+        self.weights_slope = .1
+        self.scale_weights = (1 / np.asarray(center_sigmas)) \
+            ** self.weights_slope
+
+    def sum_scales(self, filters_output):
+        # TODO: docstring
+        multiscale_output = np.tensordot(filters_output,
+                                         self.scale_weights,
+                                         (1, 0))
+        return multiscale_output
