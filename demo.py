91b5e646bc4cec9bdae3900718bf5f315cb2a7c5
Joris Vincent <joris.vincent@gmail.com>
2020-11-11 10:32:34 +0100

    Demo also plots horizontal meridians

--- demo.py
+++ demo.py
@@ -16,8 +16,22 @@ axisv = np.linspace(visextent[2], visextent[3], shape[1])
 
 # %% Circular gaussian
 img = filters.gaussian2d(x, y, (2, 2))
+
+# Plot
+plt.subplot(1, 2, 1)
 plt.imshow(img, cmap="gray")
+
+# Plot horizontal meridian
+plt.subplot(1, 2, 2)
+plt.plot(x[int(img.shape[0]/2)], img[int(img.shape[0]/2), ...])
 
 # %% Elliptical gaussian (3:1 axes; rotated)
 img = filters.gaussian2d(x, y, (6, 2), orientation=15)
+
+# Plot
+plt.subplot(1, 2, 1)
 plt.imshow(img, cmap="gray")
+
+# Plot horizontal meridian
+plt.subplot(1, 2, 2)
+plt.plot(x[int(img.shape[0]/2)], img[int(img.shape[0]/2), ...])
