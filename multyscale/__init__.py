b580f3753c963e76d6e1c427633574d924b04765
Joris Vincent <joris.vincent@gmail.com>
2021-04-30 12:27:45 +0200

    Populate __init__.py
    `import multyscale` to import all submodules as well
    `form multyscale import *` also works

---
+++ multyscale/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["filters", "filterbank", "models", "utils"]
+
+from multyscale import filters
+from multyscale import filterbank
+from multyscale import models
+from multyscale import utils
