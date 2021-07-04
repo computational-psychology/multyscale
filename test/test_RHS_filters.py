b629d4dd7cf9a05b796d8800b8a18376e8a53b5d
Joris Vincent <joris.vincent@gmail.com>
2021-07-04 15:12:12 +0200

    Turn testing scripts into Pytests

--- test/test_RHS_filters.py
+++ test/test_RHS_filters.py
@@ -1,113 +1,37 @@
 # %%
-import multyscale
 import RHS_filters
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as io
 
-# %% Create RHS filterbanks, load from Matlab
-rhs_bank = RHS_filters.filterbank()
-rhs_matlab = np.array(io.loadmat("odog_matlab.mat")["filters"].tolist())
+import pytest
+import os
 
-# %% Visualise
-for i in range(rhs_bank.shape[0]):
-    plt.subplot(rhs_bank.shape[0], 2, i * 2 + 1)
-    plt.imshow(rhs_bank[i, 6, ...])
-    plt.subplot(rhs_bank.shape[0], 2, i * 2 + 2)
-    plt.imshow(rhs_matlab[i, 6, ...])
-np.allclose(rhs_matlab, rhs_bank)
+current_dir = os.path.abspath(__file__ + "../../")
 
-# %% Parameters of image
-shape = (1024, 1024)  # filtershape in pixels
-# visual extent, same convention as pyplot:
-visextent = np.array([-0.5, 0.5, -0.5, 0.5]) * (1023 / 32)
 
-# %% Create image coordinate system:
-axish = np.linspace(visextent[0], visextent[1], shape[0])
-axisv = np.linspace(visextent[2], visextent[3], shape[1])
+@pytest.fixture()
+def rhs_bank():
+    # Create RHS filterbank from Python transplation
+    return RHS_filters.filterbank()
 
-(x, y) = np.meshgrid(axish, axisv)
 
-# %% Circular Gaussian
-sigma1 = 2
-sigmas = np.array([1, 1]) * sigma1
-f = multyscale.filters.gaussian2d(x, y, (sigmas[0], sigmas[1]))
-f = f / f.sum()
-f_2 = RHS_filters.d2gauss(shape[0], sigmas[0] * 32, shape[1], sigmas[0] * 32, 0)
+@pytest.fixture()
+def rhs_matlab_bank():
+    # Load RHS bank from MATLAB implementation
+    return np.array(
+        io.loadmat(current_dir + os.sep + "odog_matlab.mat")[
+            "filters"
+        ].tolist()
+    )
 
-plt.subplot(2, 2, 1)
-plt.imshow(f)
-plt.subplot(2, 2, 2)
-plt.imshow(f_2)
-plt.subplot(2, 2, 3)
-plt.plot(f[512, :])
-plt.subplot(2, 2, 4)
-plt.plot(f_2[512, :])
 
-np.allclose(f, f_2)
-# %% Elliptical Gaussian
-orientation = 40
-sigma2 = 2 * sigma1
-sigmas = np.array([1, 1]) * np.array([sigma1, sigma2])
-f = multyscale.filters.gaussian2d(x, y, (sigmas[0], sigmas[1]), orientation=orientation)
-f = f / f.sum()
-f_2 = RHS_filters.d2gauss(
-    shape[0], sigmas[0] * 32, shape[1], sigmas[1] * 32, orientation
-)
+def test_RHS_matlab(rhs_bank, rhs_matlab_bank):
+    # %% Visualise
+    for i in range(rhs_bank.shape[0]):
+        plt.subplot(rhs_bank.shape[0], 2, i * 2 + 1)
+        plt.imshow(rhs_bank[i, 6, ...])
+        plt.subplot(rhs_bank.shape[0], 2, i * 2 + 2)
+        plt.imshow(rhs_bank[i, 6, ...])
 
-plt.subplot(2, 2, 1)
-plt.imshow(f)
-plt.subplot(2, 2, 2)
-plt.imshow(f_2)
-plt.subplot(2, 2, 3)
-plt.plot(f[512, :])
-plt.subplot(2, 2, 4)
-plt.plot(f_2[512, :])
-
-np.allclose(f, f_2)
-# %% ODOG
-orientation = 150
-sigma3 = 2
-sigmas = np.array([[1, 1], [1, 2]]) * sigma3
-rhs_odog = RHS_filters.odog(shape[0], shape[1], sigma3 * 32, orientation=orientation)
-multy_odog = multyscale.filters.odog(
-    x, y, sigmas, orientation=(orientation, orientation)
-)
-
-plt.subplot(2, 2, 1)
-plt.imshow(rhs_odog)
-plt.subplot(2, 2, 2)
-plt.imshow(multy_odog)
-plt.subplot(2, 2, 3)
-plt.plot(rhs_odog[512, :])
-plt.subplot(2, 2, 4)
-plt.plot(multy_odog[512, :])
-
-np.allclose(rhs_odog, multy_odog)
-# %% Filterbank
-multy_bank = multyscale.filterbank.BM1999(shape, visextent)
-
-#  Visualise filterbank
-for i in range(multy_bank.filters.shape[0]):
-    plt.subplot(multy_bank.filters.shape[0], 2, i * 2 + 1)
-    plt.imshow(multy_bank.filters[i, 6, ...], extent=visextent)
-    plt.subplot(multy_bank.filters.shape[0], 2, i * 2 + 2)
-    plt.imshow(rhs_bank[i, 6, ...])
-
-np.allclose(rhs_bank, multy_bank.filters)
-
-# %%
-# Spot check
-idc_filter = (5, 6)
-plt.subplot(2, 2, 1)
-plt.imshow(rhs_bank[idc_filter])
-plt.subplot(2, 2, 2)
-plt.imshow(multy_bank.filters[idc_filter])
-plt.subplot(2, 2, 3)
-plt.plot(rhs_bank[idc_filter][512, :])
-plt.subplot(2, 2, 4)
-plt.plot(multy_bank.filters[idc_filter][512, :])
-
-# %
-np.allclose(rhs_bank[idc_filter], multy_bank.filters[idc_filter])
-# %%
+    assert np.allclose(rhs_bank, rhs_bank)
