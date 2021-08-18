52450483256f9a0601f21bdc5648ea7f787f15c5
Joris Vincent <joris.vincent@gmail.com>
2021-08-18 20:13:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="multyscale",
-    version="0.1",
+    version="0.2",
     description="Multiscale spatial filtering",
     author="Joris Vincent",
     author_email="joris.vincent@tu-berlin.de",
     license="MIT",
-    packages=["multyscale"],
+    package_dir={"stimuli": "stimuli"},
+    packages=find_packages(),
     zip_safe=False,
 )
