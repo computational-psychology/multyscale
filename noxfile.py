41543a4dd95702ba72a0bde3fdb45fed3fcfd9ce
Joris Vincent <joris.vincent@tu-berlin.de>
2024-02-29 20:44:53 +0100

    CI: GitHub Action for testing
    runs pytest on multiple platforms, python versions, through Nox
    only runs on PRs, push to main, and manual triggers

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,12 @@
 import nox
 
 
+@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
+def tests(session):
+    session.install(".[dev]")
+    session.run("pytest")
+
+
 @nox.session(python="3.12")
 def lint(session):
     # Run the linters (via pre-commit)
