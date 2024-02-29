b43888919afaea321ee4123fe98aa4b5c976c3fb
Joris Vincent <joris.vincent@tu-berlin.de>
2024-02-29 17:39:24 +0100

    test(filters): parametrize
    circular Gaussian vs. elliptical Gaussian is only a change in parametrization

--- test/test_filters.py
+++ test/test_filters.py
@@ -1,75 +1,69 @@
 import numpy as np
+import pytest
 import RHS_implementation
 
 import multyscale
 
-# %% RHS bank
-RHS_bank = RHS_implementation.filterbank()
-
-# %% Parameters of image
-shape = (1024, 1024)  # filtershape in pixels
+# %% Global parametrization
+# Parameters of image
+shape = (1024, 1024)  # in pixels
+PPD = 32
 # visual extent, same convention as pyplot:
-visextent = np.array([-0.5, 0.5, -0.5, 0.5]) * (1023 / 32)
+visextent = np.array([-0.5, 0.5, -0.5, 0.5]) * (1023 / PPD)
 
-# %% Create image coordinate system:
+# Image coordinate system
 axish = np.linspace(visextent[0], visextent[1], shape[0])
 axisv = np.linspace(visextent[2], visextent[3], shape[1])
-
 (x, y) = np.meshgrid(axish, axisv)
 
-
-def test_circular_Gaussian():
-    """Test that multyscale's (circular) Gaussian is equal to RHS (circular) Gaussian"""
-
-    # Filter params
-    sigma1 = 2
-    sigmas = np.array([1, 1]) * sigma1
-
-    # Generate filter
-    filter_multy = multyscale.filters.gaussian2d(x, y, (sigmas[0], sigmas[1]))
-    filter_multy = filter_multy / filter_multy.sum()
-
-    # Generate comparison filter
-    filter_multy = RHS_implementation.d2gauss(
-        shape[0], sigmas[0] * 32, shape[1], sigmas[0] * 32, 0
-    )
-
-    assert np.allclose(filter_multy, filter_multy)
+pytestmark = [pytest.mark.parametrize("sigma_center", RHS_implementation.std)]
 
 
-def test_elliptical_Gaussian():
-    """Test that multyscale's (elliptical) Gaussian is equal to RHS (elliptical) Gaussian"""
+# %% TEST
+@pytest.mark.parametrize(
+    "sigma_ratio, orientation",
+    [
+        (1, 0),  # circular, no need for orientation
+        (2, 0),  # elliptical, cycle through all orientations
+        (2, 30),
+        (2, 60),
+        (2, 90),
+        (2, 120),
+        (2, 150),
+    ],
+)
+def test_Gaussian(sigma_ratio, sigma_center, orientation):
+    """Test that multyscale's Gaussian is equal to RHS Gaussian"""
 
     # Filter params
-    sigma1 = 2
-    orientation = 40
-    sigma2 = 2 * sigma1
-    sigmas = np.array([1, 1]) * np.array([sigma1, sigma2])
+    sigmas = np.array([1, sigma_ratio]) * sigma_center
 
     # Generate filter
     filter_multy = multyscale.filters.gaussian2d(
         x, y, (sigmas[0], sigmas[1]), orientation=orientation
     )
-    filter_multy = filter_multy / filter_multy.sum()
+    filter_multy /= filter_multy.sum()
 
     # Generate comparison filter
-    filter_RHS = RHS_implementation.d2gauss(
-        shape[0], sigmas[0] * 32, shape[1], sigmas[1] * 32, orientation
+    filter_multy = RHS_implementation.d2gauss(
+        shape[0], sigmas[0] * PPD, shape[1], sigmas[0] * PPD, orientation
     )
 
-    assert np.allclose(filter_multy, filter_RHS)
+    assert np.array_equal(filter_multy, filter_multy)
 
 
-def test_ODOG():
+@pytest.mark.parametrize("sigma_ratio", [2])
+@pytest.mark.parametrize("orientation", [0, 30, 60, 90, 120, 150])
+def test_ODOG(sigma_center, sigma_ratio, orientation):
     """Test that multyscale's ODOG filter is equal to RHS ODOG filter"""
 
     # Filter params
-    orientation = 150
-    sigma3 = 2
-    sigmas = np.array([[1, 1], [1, 2]]) * sigma3
+    sigmas = np.array([[1, 1], [1, sigma_ratio]]) * sigma_center
 
     # Generate filter
-    filter_RHS = RHS_implementation.odog(shape[0], shape[1], sigma3 * 32, orientation=orientation)
+    filter_RHS = RHS_implementation.odog(
+        shape[0], shape[1], sigma_center * PPD, orientation=orientation
+    )
 
     # Generate comparison filter
     filter_multy = multyscale.filters.odog(x, y, sigmas, orientation=(orientation, orientation))
